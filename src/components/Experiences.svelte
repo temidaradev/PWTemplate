ec8c85b147e5b6137a4f5e3391d8e4b988b61bf4
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2024-11-14 20:18:23 +0300

    experience upgrade

--- src/components/Experiences.svelte
+++ src/components/Experiences.svelte
@@ -4,7 +4,15 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-  <section id="introPage" class="grid grid-cols-1 py-8 sm:py-14">
-    <h1>Enter Your Experiences Or List What you did</h1>
-  </section>
+  <div>
+    <h2 class="font-bold text-xl sm:text-2xl md:text-3xl text-violet-400">
+      Field You Working In
+    </h2>
+    <h3 class="font-semibold text-m sm:text-l md:text-xl">
+      Im currently working for <a href="#">company that you are working for</a> and
+      how you are working.
+    </h3>
+    <h3 class="text-s sm:text-m md:text-l">Date - Date</h3>
+    <hr class="h-1.5 my-2 bg-gray-200 border-0 dark:bg-violet-700" />
+  </div>
 </main>
